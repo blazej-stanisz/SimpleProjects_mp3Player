a93031f6b40264eb8d2797b583604d5d724ca565
Błażej Stanisz <blazej.stanisz@intel.com>
2020-04-26 21:36:51 +0200

    adding files to playlist

---
+++ mp3Player/ListViewAudioItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mp3Player
+{
+    public class ListViewAudioItem
+    {
+        public string FileName { get; set; }
+
+        public string FileFullPath { get; set; }
+
+        public string FileDurationString { get; set; }
+
+        public int FileDurationInSeconds{ get; set; }
+    }
+}
--- mp3Player/MainWindow.xaml.cs
+++ mp3Player/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,6 +22,9 @@ namespace mp3Player
     /// </summary>
     public partial class MainWindow : Window
     {
+        private bool isPaused = false;
+        private List<string> audioFiles = new List<string>();
+
         public MainWindow()
         {
             InitializeComponent();
@@ -28,11 +33,74 @@ namespace mp3Player
         private void PlayBtn_Click(object sender, RoutedEventArgs e)
         {
             MusicPlayerMediaElement.Play();
+            isPaused = false;
         }
 
         private void MusicPlayerMediaElement_MediaFailed(object sender, ExceptionRoutedEventArgs e)
         {
             int a = 10;
+        }
+
+        private void StopBtn_Click(object sender, RoutedEventArgs e)
+        {
+            MusicPlayerMediaElement.Stop();
+        }
+
+        private void PauseBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (isPaused)
+            {
+                MusicPlayerMediaElement.Play();
+                isPaused = false;
+            }
+            else
+            {
+                MusicPlayerMediaElement.Pause();
+                isPaused = true;
+            }
+        }
+
+        private void OpenFileDialogBtn_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "All Supported Audio | *.mp3; *.wma | MP3s | *.mp3 | WMAs | *.wma";
+            openFileDialog.Multiselect = true;
+            openFileDialog.ShowDialog();
+
+            audioFiles = openFileDialog.FileNames.ToList();
+            preparePlaylistPanel();
+        }
+
+        private void preparePlaylistPanel()
+        {
+            //List<ListViewAudioItem> preparedAudioList = new List<ListViewAudioItem>();
+
+            foreach (var audioFile in audioFiles)
+            {
+                TagLib.File f = TagLib.File.Create(audioFile, TagLib.ReadStyle.Average);
+                var durationSeconds = (int)f.Properties.Duration.TotalSeconds;
+
+
+                TimeSpan t = TimeSpan.FromSeconds(durationSeconds);
+                var durationString = string.Format("{0:D2}h:{1:D2}m:{2:D2}s",
+                                t.Hours,
+                                t.Minutes,
+                                t.Seconds);
+
+                var item = new ListViewAudioItem { 
+                    FileFullPath = audioFile, 
+                    FileName = System.IO.Path.GetFileName(audioFile), 
+                    FileDurationInSeconds = durationSeconds,
+                    FileDurationString = durationString
+                };
+
+                audioPlaylistListView.Items.Add(item);
+                //preparedAudioList.
+            }
+
+            //audioPlaylistListView.Items.a .AddpreparedAudioList);
+
+            var a = 10;
         }
     }
 }
